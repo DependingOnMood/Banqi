371e7de64ac7e76e282d180b8d28c20695e4df59
Dongbo Xiao <xiaodongbo627@gmail.com>
2015-05-19 01:25:07 -0400

    resize help screen to fit in the app again

--- languages/en.js
+++ languages/en.js
@@ -3,7 +3,7 @@ window.angularTranslationsLoaded('en',{
 "RULES_SLIDE1":"The ranking goes as follows: general, advisor, elephant, chariot, horse, soldier. Only pieces of equal or lower rank may be captured, with one exception. The one exception concerns generals and soldiers: the general cannot capture soldiers, and soldiers can capture the general.",
 "RULES_SLIDE2":"Except for the cannon, pieces capture with the same motion as for movement: one square up, down, left, or right.",
 "RULES_SLIDE3":"The cannon is not included in the ranking because it is exceptional: it captures in an unusual way, it can capture a piece of any rank, and yet is vulnerable to capture by any piece except the soldier.",
-"RULES_SLIDE4":"A cannon captures as in xiangqi: it moves any distance along a single row or column of the board, jumping over exactly one intermediate piece (called a screen). Any other squares between the cannon and its target must be empty. The color of the screening piece does not matter; it may be friend or foe, or even face down. While a cannon may capture any piece, it must jump a screen to do so. Since a cannon must jump to capture, it cannot capture a piece in an adjacent square.",
+"RULES_SLIDE4":"A cannon captures is: moves any distance along a single row or column of the board, jumping over exactly one intermediate piece. Any other squares between the cannon and its target must be empty. Since a cannon must jump to capture, it cannot capture a piece in an adjacent square.",
 "RULES_SLIDE5":"Red has the 1st move. The player killed all pieces in other side win the game.",
 "CLOSE":"Close"
 });
